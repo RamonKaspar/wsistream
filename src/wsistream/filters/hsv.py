fix: optimize pixel fraction calculation in HSVPatchFilter acceptance method

src/wsistream/filters/hsv.py
@@ -51,5 +51,5 @@ class HSVPatchFilter(PatchFilter):
         lower = np.array([self.hue_range[0], self.sat_range[0], self.val_range[0]])
         upper = np.array([self.hue_range[1], self.sat_range[1], self.val_range[1]])
         in_range = cv2.inRange(hsv, lower, upper)
-        fraction = float(in_range.astype(bool).sum()) / in_range.size
+        fraction = np.count_nonzero(in_range) / in_range.size
         return fraction >= self.min_pixel_fraction